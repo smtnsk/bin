f09db347cec47203b5c1aa81b2e1de40c5c1bada
Sami Tanskanen <github@wilywily.com>
2021-09-08 21:51:00 +0300

    Add comp_path

--- completions/detach.bash
+++ completions/detach.bash
@@ -1,20 +1,2 @@
-_detach()
-{
-	if [ "${#COMP_WORDS[@]}" != "2" ]; then
-		return
-	fi
-
-	## Strip : from $PATH
-	IFS_old=$IFS;
-	IFS=":";
-	set -- $PATH;
-	IFS=$IFS_old;
-
-	for dir; do
-		bins="$bins $(cd "$dir" && compgen -f)"
-	done
-
-	COMPREPLY=($(compgen -W "$bins" "${COMP_WORDS[1]}"))
-}
-
-complete -F _detach detach
+. comp_path.bash
+complete -F _comp_path detach
